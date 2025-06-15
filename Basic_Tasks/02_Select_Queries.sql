4458b8835435947d05225aa81db5606b3a226d8c
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-15 22:49:59 +0500

    Added basic SELECT queries to retrieve employee names, positions, and department locations for basic-level internship tasks.

---
+++ Basic_Tasks/02_Select_Queries.sql
@@ -0,0 +1,7 @@
+USE Internship_DB;
+
+-- Retrieve employees' names and positions
+SELECT Name, Position FROM Employees;
+
+-- List departments with locations
+SELECT DepartmentName, Location FROM Departments;
